561fd8400c2c7ee337dd2e2fbb4def6989abb89d
PyLukasz <as.lukasz@gmail.com>
2022-10-03 22:29:25 +0200

    Add EF to project, configure connection.

---
+++ MusicShop/Data/MusicShopContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicShop.Models;
+
+namespace MusicShop.Data
+{
+    public class MusicShopContext : DbContext
+    {
+        private string _connectionString =
+           "Server=(localdb)\\MSSQLLocalDB;Database=MusicShopDb;Trusted_Connection=True;";
+        public MusicShopContext()
+        {
+
+        }
+        public MusicShopContext(DbContextOptions<MusicShopContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Album>? Albums { get; set; }
+        public DbSet<Artist>? Artists { get; set; }
+        public DbSet<Genre>? Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Album>().Property(a => a.Price).HasColumnType("decimal(4,2)");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
