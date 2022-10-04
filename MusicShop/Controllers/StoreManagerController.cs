a526672f89f3fec00d075c153f875e055acf6195
PyLukasz <as.lukasz@gmail.com>
2022-10-04 16:04:20 +0200

    Add store manager controller with CRUD operations.

---
+++ MusicShop/Controllers/StoreManagerController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MusicShop.Data;
+using MusicShop.Models;
+
+namespace MusicShop.Controllers
+{
+    public class StoreManagerController : Controller
+    {
+        private readonly MusicShopContext _context;
+
+        public StoreManagerController(MusicShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: StoreManager
+        public async Task<IActionResult> Index()
+        {
+            var musicShopContext = _context.Albums.Include(a => a.Artist).Include(a => a.Genre);
+            return View(await musicShopContext.ToListAsync());
+        }
+
+        // GET: StoreManager/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Albums == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albums
+                .Include(a => a.Artist)
+                .Include(a => a.Genre)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // GET: StoreManager/Create
+        public IActionResult Create()
+        {
+            ViewData["ArtistId"] = new SelectList(_context.Artists, "Id", "Id");
+            ViewData["GenreId"] = new SelectList(_context.Genres, "Id", "Id");
+            return View();
+        }
+
+        // POST: StoreManager/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,GenreId,ArtistId,Title,Price,AlbumArtUrl")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(album);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ArtistId"] = new SelectList(_context.Artists, "Id", "Id", album.ArtistId);
+            ViewData["GenreId"] = new SelectList(_context.Genres, "Id", "Id", album.GenreId);
+            return View(album);
+        }
+
+        // GET: StoreManager/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Albums == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            ViewData["ArtistId"] = new SelectList(_context.Artists, "Id", "Id", album.ArtistId);
+            ViewData["GenreId"] = new SelectList(_context.Genres, "Id", "Id", album.GenreId);
+            return View(album);
+        }
+
+        // POST: StoreManager/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,GenreId,ArtistId,Title,Price,AlbumArtUrl")] Album album)
+        {
+            if (id != album.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(album);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlbumExists(album.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ArtistId"] = new SelectList(_context.Artists, "Id", "Id", album.ArtistId);
+            ViewData["GenreId"] = new SelectList(_context.Genres, "Id", "Id", album.GenreId);
+            return View(album);
+        }
+
+        // GET: StoreManager/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Albums == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albums
+                .Include(a => a.Artist)
+                .Include(a => a.Genre)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // POST: StoreManager/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Albums == null)
+            {
+                return Problem("Entity set 'MusicShopContext.Albums'  is null.");
+            }
+            var album = await _context.Albums.FindAsync(id);
+            if (album != null)
+            {
+                _context.Albums.Remove(album);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlbumExists(int id)
+        {
+          return _context.Albums.Any(e => e.Id == id);
+        }
+    }
+}
