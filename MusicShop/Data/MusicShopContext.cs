2a9b49a45c5da68d86966a808933fbc1aaef62ce
PyLukasz <as.lukasz@gmail.com>
2022-10-04 19:38:53 +0200

    Add models to manage order: cart, order, orderdetail

--- MusicShop/Data/MusicShopContext.cs
+++ MusicShop/Data/MusicShopContext.cs
@@ -18,6 +18,9 @@ namespace MusicShop.Data
         public DbSet<Album>? Albums { get; set; }
         public DbSet<Artist>? Artists { get; set; }
         public DbSet<Genre>? Genres { get; set; }
+        public DbSet<Cart>? Carts { get; set; }
+        public DbSet<Order>? Orders { get; set; }
+        public DbSet<OrderDetail>? OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
